cedaeb95868bf2fe6364ef3bcaf47c328dc03b02
Rafael Quintela <rafael4sq@gmail.com>
2018-10-27 13:38:11 -0300

    yarn basics & neovim

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
+echo "neovim"
+sudo apt-add-repository ppa:neovim-ppa/stable
+
 echo "yarn https://yarnpkg.com/pt-BR/docs/install"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-echo "deps"
-sudo apt update && sudo apt install git bison gcc make zsh silversearcher-ag autojump yarn
+echo "install"
+sudo apt update
+sudo apt install git bison gcc make zsh silversearcher-ag autojump yarn neovim python-dev python-pip python3-dev python3-pip
 
 echo "oh-my-zsh, https://github.com/robbyrussell/oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -12,5 +16,5 @@ echo "plug - https://github.com/junegunn/vim-plug"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "parcel"
-yarn global add parcel
+echo "yarn basics"
+yarn global add parcel eslint prettier-eslint
