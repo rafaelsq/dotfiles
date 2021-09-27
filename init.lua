c1b0b162c6b745a2706e17695f5c454d9a80dd60
Rafael Quintela <rafael4sq@gmail.com>
2021-09-27 10:15:28 -0300

    nvim - current dir only the first time

--- init.lua
+++ init.lua
@@ -296,12 +296,16 @@ end
 
 -- Use an on_attach function to only map the following keys 
 -- after the language server attaches to the current buffer
+local setDir = false
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   -- use project root_dir
-  vim.api.nvim_set_current_dir(client.config.root_dir)
+  if not setDir then
+    vim.api.nvim_set_current_dir(client.config.root_dir)
+    setDir = true
+  end
 
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
