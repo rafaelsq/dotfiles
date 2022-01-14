df86955d4343fd8543fd2033943f84fe23e98365
Rafael Quintela <rafael4sq@gmail.com>
2022-01-14 15:15:26 -0300

    fix offset_encoding

--- init.lua
+++ init.lua
@@ -301,15 +301,20 @@ local lsp = require'lspconfig'
 
 -- https://github.com/neovim/nvim-lspconfig/issues/115
 function org_imports(wait_ms)
-  local params = vim.lsp.util.make_range_params()
-  params.context = {only = {"source.organizeImports"}}
-  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-  for _, res in pairs(result or {}) do
-    for _, r in pairs(res.result or {}) do
-      if r.edit then
-        vim.lsp.util.apply_workspace_edit(r.edit)
-      else
-        vim.lsp.buf.execute_command(r.command)
+  local clients = vim.lsp.buf_get_clients()
+  for _, client in pairs(clients) do
+
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
       end
     end
   end
